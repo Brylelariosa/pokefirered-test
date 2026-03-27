1828cc6d15bcc2129952857845067a41314d84bf
MangaTracker <brylelariosa12345@gmail.com>
2026-03-27 11:48:16 +0800

    Create gimmick.h

---
+++ include/constants/gimmick.h
@@ -0,0 +1,8 @@
+#ifndef GUARD_CONSTANTS_GIMMICK_H
+#define GUARD_CONSTANTS_GIMMICK_H
+
+#define GIMMICK_MOVE_SLOT     4        // index of the 5th move
+#define GIMMICK_USED_FLAG     0x800    // flag = move already used this battle
+#define MAX_MON_MOVES_GIMMICK 5        // expanded move count
+
+#endif
